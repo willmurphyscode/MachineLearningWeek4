191a31a2b68e54ab21d275c81152eea40aa8defc
William Murphy <willmurphyscode@gmail.com>
2016-10-20 07:30:12 -0400

    model attempt only using complete cases

---
+++ makeModels2.R
@@ -0,0 +1,9 @@
+completeTrain <- trainSet[complete.cases(trainSet),]
+completeTrain$classe <- factor(completeTrain$classe)
+only1 <- apply(completeTrain, 2, function(x) { length(unique(x)) == 1})
+
+completeTrain <- completeTrain[,which(only1) := NULL]
+
+mod <- caret::train(classe ~ ., data=completeTrain, method="rpart")
+
+head(completeTrain)
