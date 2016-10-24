45836fe5bdc95788436dc624f1693273e17f3b66
William Murphy <willmurphyscode@gmail.com>
2016-10-24 19:46:56 -0400

    finally have a workable model

--- transformRow.R
+++ transformRow.R
@@ -6,14 +6,48 @@ require(dplyr)
 #observations per window, and the data that is observed continuously
 #will be transformed.
 
+
+
+meanNumerics <- function(input) {
+  result <- NULL
+  if(is.numeric(input)) {
+    result <- mean(input, na.rm = TRUE)
+  } else {
+    result <- input[1]
+  }
+  
+  result
+  
+}
+
+makeIgnorableColumns <- function(dt) {
+  ignoreMe <- NULL
+  sadfaces <- apply(dt, 2 , function(x) { all(is.na(x))})
+  
+  ignoreMe <- colnames(dt)[which(sadfaces)]
+  ignoreMe
+}
+
 transformData <- function(dt, vecColNamesToIgnore, fnSummarizeNumeric = function(x) {mean(x, na.rm = TRUE)}) {
     tmpDt <- dt#[, which(colnames(dt) %in% vecColNamesToIgnore) := NULL, with = FALSE]
-    print(str(dt))
+    #print(str(dt))
 
     tmpDt <-tmpDt %>% 
-        group_by(tmpDt$num_window) 
-        %>% summarise_each(funs(min, max))
+        group_by(num_window) %>%
+          summarise_each(funs(meanNumerics(.)))
 
-    
-
+    tmpDt <- tmpDt[, which(colnames(tmpDt) %in% vecColNamesToIgnore) := NULL, with = FALSE]
 }
+
+
+doTransform <- function() {
+  colNamesToIgnore <- makeIgnorableColumns(trainSet)
+  result <- transformData(trainSet, colNamesToIgnore)
+  
+  result
+}
+
+mod <- caret::train(classe ~ ., data = completeTrain, method="rpart")
+
+rattle::fancyRpartPlot(mod$finalModel)
+
