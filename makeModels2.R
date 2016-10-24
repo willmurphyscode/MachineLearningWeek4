492da7515e8bfb6879b3da3b1f79d1e0493454bf
William Murphy <willmurphyscode@gmail.com>
2016-10-24 08:09:49 -0400

    backup scratchwork document

--- makeModels2.R
+++ makeModels2.R
@@ -1,9 +1,17 @@
 completeTrain <- trainSet[complete.cases(trainSet),]
 completeTrain$classe <- factor(completeTrain$classe)
-only1 <- apply(completeTrain, 2, function(x) { length(unique(x)) == 1})
+only1 <- apply(trainSet, 2, function(x) { length(unique(x)) == 1})
+delme <- (which(only1))
+completeTrain <- completeTrain[,names(delme) := NULL]
 
-completeTrain <- completeTrain[,which(only1) := NULL]
+trainSet2 <- trainSet[, names(delme) := NULL] 
+trainMat <- model.matrix(classe ~ ., data = trainSet2)
 
-mod <- caret::train(classe ~ ., data=completeTrain, method="rpart")
+mod <- caret::train(classe ~ ., data=trainSet2, 
+                    method="rpart"
+                    , na.action = na.omit, preProcess = "zv"
+                    )
+beepr::beep()
+printcp(mod$finalModel) 
 #rattle::printRandomForests(mod)
 head(completeTrain)
